fcdaa7177695d66a4e1e81dd2ef4a873be8c5864
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-04-11 09:40:30 +1000

    Added a second character.

--- modules/TestLevel/main.cs
+++ modules/TestLevel/main.cs
@@ -11,7 +11,7 @@ function TestLevel::create(%this) {
    }
 
    TheSceneWindow.setCameraPosition(0, 0);
-   TheSceneWindow.setCameraSize(10, 7.5);
+   TheSceneWindow.setCameraSize(20, 15);
    TheSceneWindow.setCameraZoom(1);
    TheSceneWindow.setCameraAngle(0);
 
@@ -37,18 +37,25 @@ function TestLevel::createRect(%w, %h) {
    %rect = new ShapeVector();
    %rect.setPolyPrimitive(4);
    %rect.setSize(%w SPC %h);
-   %rect.setBodyType("static");
+   %rect.setBodyType(static);
    %rect.createPolygonBoxCollisionShape(%w, %h);
    return %rect;
 }
 
-function TestLevel::construct(%this) {
-   // Create some platforms.
-   %wall = TestLevel::createRect(5, 1);
-   %wall.position = "0 -2";
-   %this.add(%wall);
+function TestLevel::construct(%scene) {
+   %scene.setGravity(0, -9.8);
 
-   %player = PlatformCharacter::spawn();
+   // Create some platforms.
+   %wall = TestLevel::createRect(15, 1);
+   %wall.position = "0 -2";
+   %scene.add(%wall);
+
+   // Main character
+   %player = PlatformCharacter::spawn("primary");
    %player.position = "0 2";
-   %this.add(%player);
+   %scene.add(%player);
+   // And another
+   %buddy = PlatformCharacter::spawn("secondary");
+   %buddy.position = "2 2";
+   %scene.add(%buddy);
 }
