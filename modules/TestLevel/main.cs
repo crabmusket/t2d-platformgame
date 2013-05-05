1147ed16ef589bcf7db56721077e769b5ba78b6a
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-05-06 09:07:32 +1000

    Added damage text popups.

--- modules/TestLevel/main.cs
+++ modules/TestLevel/main.cs
@@ -65,17 +65,42 @@ function TestLevel::construct(%scene) {
       %i++;
    }
 
+   // Damage effects
+   %e = Damage.createDefendEffect(HitTextBehavior);
+
    // Main character
    %player = PlatformCharacter::spawn("primary");
    %player.position = 0 SPC %walls[mFloor(%num/2)].getSizeY() + 2;
+   Damage.effect(%player, %e);
    %scene.add(%player);
    // And another
    %buddy = PlatformCharacter::spawn("secondary");
    %buddy.position = 2 SPC %walls[mFloor(%num/2)].getSizeY() + 2;
+   Damage.effect(%buddy, %e);
    %scene.add(%buddy);
 
    // Tracking camera
    TrackingCamera.addToWindow(TheSceneWindow);
    TrackingCamera.track(%player);
    TrackingCamera.track(%buddy);
+}
+
+function HitTextBehavior::onDamaged(%this, %attacker, %types, %amount, %impulse) {
+   %t = new ImageFont();
+
+   %t.image = "TestLevel:font";
+   %t.fontSize = "0.2 0.4";
+   %t.fontPadding = 0;
+   %t.textAlignment = "Center";
+   %t.text = %amount;
+
+   %t.bodyType = "dynamic";
+   %t.gravityScale = 0.3;
+   %t.linearVelocity = "0 3";
+   %t.position = %this.owner.getPosition();
+
+   %t.schedule(1000, safeDelete);
+   %this.owner.getScene().add(%t);
+
+   Parent::onDamage(%this, %attacker, %defender, %types, %amount, %impulse);
 }
